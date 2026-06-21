1c73d9e8c8e6cd0c80523a015c626b2f81a7c851
Harsh Narayan Jha <harshnj@proton.me>
2026-06-21 16:33:34 +0530

    deps: bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ import org.jetbrains.kotlin.konan.properties.Properties
 import java.io.FileInputStream
 
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization)
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.compose)
+  alias(libs.plugins.kotlin.serialization)
 }
 
 val keystorePropertiesFile = rootProject.file("key.properties")
@@ -28,41 +28,41 @@ secretProdProperties.load(FileInputStream(secretProdPropertiesFile))
 
 
 android {
-    namespace = "com.freyza.employee"
-    compileSdk = 36
+  namespace = "com.freyza.employee"
+  compileSdk = 36
 
-    defaultConfig {
-        applicationId = "com.freyza.employee"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+  defaultConfig {
+    applicationId = "com.freyza.employee"
+    minSdk = 24
+    targetSdk = 36
+    versionCode = 1
+    versionName = "1.0.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-    buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "")
-    buildConfigField("String", "SUPABASE_URL", "")
-    buildConfigField("String", "WEB_CLIENT_ID", "")
-    buildConfigField("String", "API_URL", "")
+    buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\"\"")
+    buildConfigField("String", "SUPABASE_URL", "\"\"")
+    buildConfigField("String", "WEB_CLIENT_ID", "\"\"")
+    buildConfigField("String", "API_URL", "\"\"")
   }
 
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
-        }
+  signingConfigs {
+    create("release") {
+      keyAlias = keystoreProperties["keyAlias"] as String
+      keyPassword = keystoreProperties["keyPassword"] as String
+      storeFile = file(keystoreProperties["storeFile"] as String)
+      storePassword = keystoreProperties["storePassword"] as String
     }
+  }
 
-    flavorDimensions += "env"
+  flavorDimensions += "env"
 
-    productFlavors {
-        create("dev") {
-            dimension = "env"
-            applicationIdSuffix = ".dev"
-            versionNameSuffix = "-dev"
-            resValue("string", "app_name", "Freyza Employee (Dev)")
+  productFlavors {
+    create("dev") {
+      dimension = "env"
+      applicationIdSuffix = ".dev"
+      versionNameSuffix = "-dev"
+      resValue("string", "app_name", "Freyza Employee (Dev)")
 
       buildConfigField(
         "String",
@@ -86,11 +86,11 @@ android {
       )
     }
 
-        create("preview") {
-            dimension = "env"
-            applicationIdSuffix = ".preview"
-            versionNameSuffix = "-pre"
-            resValue("string", "app_name", "Freyza Employee (Preview)")
+    create("preview") {
+      dimension = "env"
+      applicationIdSuffix = ".preview"
+      versionNameSuffix = "-pre"
+      resValue("string", "app_name", "Freyza Employee (Preview)")
 
       buildConfigField(
         "String",
@@ -114,8 +114,8 @@ android {
       )
     }
 
-        create("prod") {
-            dimension = "env"
+    create("prod") {
+      dimension = "env"
 
       buildConfigField(
         "String",
@@ -140,82 +140,82 @@ android {
     }
   }
 
-    buildTypes {
-        getByName("debug") {
-            isDebuggable = true
-            versionNameSuffix = "-debug"
-        }
-        getByName("release") {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            isDebuggable = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-
-            signingConfig = signingConfigs.getByName("release")
-        }
+  buildTypes {
+    getByName("debug") {
+      isDebuggable = true
+      versionNameSuffix = "-debug"
     }
+    getByName("release") {
+      isMinifyEnabled = true
+      isShrinkResources = true
+      isDebuggable = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+      signingConfig = signingConfigs.getByName("release")
     }
+  }
 
-    buildFeatures {
-        compose = true
-        buildConfig = true
-        resValues = true
-    }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
 
-    buildToolsVersion = "36.0.0"
+  buildFeatures {
+    compose = true
+    buildConfig = true
+    resValues = true
+  }
+
+  buildToolsVersion = "36.0.0"
 }
 
 dependencies {
-    // core android and compose
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.runtime.livedata)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+  // core android and compose
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.compose.ui)
+  implementation(libs.androidx.compose.ui.graphics)
+  implementation(libs.androidx.compose.ui.tooling.preview)
+  implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.compose.runtime.livedata)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+  debugImplementation(libs.androidx.compose.ui.tooling)
+  debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    // compose navigation
-    implementation(libs.kotlinx.serialization.json)
-    implementation(libs.androidx.navigation.compose)
+  // compose navigation
+  implementation(libs.kotlinx.serialization.json)
+  implementation(libs.androidx.navigation.compose)
 
-    // koin DI
-    implementation(platform(libs.koin.bom))
-    implementation(libs.koin.core)
-    implementation(libs.koin.android)
-    implementation(libs.koin.compose)
+  // koin DI
+  implementation(platform(libs.koin.bom))
+  implementation(libs.koin.core)
+  implementation(libs.koin.android)
+  implementation(libs.koin.compose)
 
-    // supabase
-    implementation(platform(libs.supabase.bom))
-    implementation(libs.auth.kt)
-    implementation(libs.postgrest.kt)
+  // supabase
+  implementation(platform(libs.supabase.bom))
+  implementation(libs.auth.kt)
+  implementation(libs.postgrest.kt)
 
-    implementation(libs.ktor.client.android)
-    implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.ktor.client.logging)
+  implementation(libs.ktor.client.android)
+  implementation(libs.ktor.client.content.negotiation)
+  implementation(libs.ktor.serialization.kotlinx.json)
+  implementation(libs.ktor.client.logging)
 
-    // google login
-    implementation(libs.googleid)
-    implementation(libs.androidx.credentials)
-    implementation(libs.androidx.credentials.play.services.auth)
+  // google login
+  implementation(libs.googleid)
+  implementation(libs.androidx.credentials)
+  implementation(libs.androidx.credentials.play.services.auth)
 
-    implementation(libs.kizitonwose.calendar)
-//    implementation(libs.timber)
+  implementation(libs.kizitonwose.calendar)
+  implementation(libs.timber)
 }
