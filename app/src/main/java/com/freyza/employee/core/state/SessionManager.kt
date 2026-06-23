0c989e59585eb74c619852a6a1fe5e44b20154fc
Harsh Narayan Jha <harshnj@proton.me>
2026-06-23 16:31:46 +0530

    setup timber and sentry logging
    - migrate application logging to Timber
    - add Sentry integration for preview and production builds
    - add initial user and route tagging
    - disable Sentry in debug builds

--- app/src/main/java/com/freyza/employee/core/state/SessionManager.kt
+++ app/src/main/java/com/freyza/employee/core/state/SessionManager.kt
@@ -1,7 +1,9 @@
 package com.freyza.employee.core.state
 
+import com.freyza.employee.BuildConfig
 import com.freyza.employee.core.util.Logger
 import com.freyza.employee.domain.model.User
+import io.sentry.Sentry
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
@@ -10,20 +12,46 @@ import kotlinx.coroutines.flow.asStateFlow
  */
 class SessionManager {
 
-    companion object {
-        const val TAG = "SessionManager"
-    }
+  companion object {
+    const val TAG = "SessionManager"
+  }
 
-    private val _currentEmployee = MutableStateFlow<User?>(null)
-    val currentEmployee = _currentEmployee.asStateFlow()
+  private val _currentEmployee = MutableStateFlow<User?>(null)
+  val currentEmployee = _currentEmployee.asStateFlow()
 
-    fun setCurrentEmployee(employee: User?) {
-        _currentEmployee.value = employee
-        Logger.i(TAG, "Employee set to $employee")
-    }
+  fun setCurrentEmployee(employee: User?) {
+    _currentEmployee.value = employee
+    Logger.i(TAG, "Employee set to ${employee?.id}")
 
-    fun clearSession() {
-        _currentEmployee.value = null
-        Logger.i(TAG, "Session was cleared")
+    if (!BuildConfig.DEBUG) {
+      if (employee != null) {
+        val sentryUser = io.sentry.protocol.User().apply {
+          id = employee.id
+          email = employee.email
+          username = employee.name
+        }
+        Sentry.setUser(sentryUser)
+
+        Sentry.setTag("user.role", employee.role.name)
+        employee.hqId?.let { Sentry.setTag("user.hq_id", it) }
+      } else {
+        sentryClearUser()
+      }
     }
+  }
+
+  fun clearSession() {
+    _currentEmployee.value = null
+    Logger.i(TAG, "Session was cleared")
+
+    if (!BuildConfig.DEBUG) {
+      sentryClearUser()
+    }
+  }
+
+  private fun sentryClearUser() {
+    Sentry.setUser(null)
+    Sentry.removeTag("user.role")
+    Sentry.removeTag("user.hq_id")
+  }
 }
